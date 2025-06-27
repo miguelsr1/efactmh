47393af40e283f4eedcc8c7b5fff3db52e522d8f
msanchez <miguelsr1@gmail.com>
2025-06-27 00:12:02 -0600

    representante del emisor

--- src/main/java/sv/com/jsoft/efactmh/model/Emisor.java
+++ src/main/java/sv/com/jsoft/efactmh/model/Emisor.java
@@ -9,9 +9,13 @@ import sv.com.jsoft.efactmh.model.dto.EmisorDto;
  * @author migue
  */
 @Data
-@EqualsAndHashCode(callSuper=false)
-public class Emisor extends EmisorDto{
+@EqualsAndHashCode(callSuper = false)
+public class Emisor extends EmisorDto {
 
     private String codigoDepartamento;
     private String codigoMunicipio;
+
+    private String nombreResponsable;
+    private String tipoDocResponsable;
+    private String numeroDocResponsable;
 }
