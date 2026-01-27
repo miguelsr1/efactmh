e457f8dcecd92ffbc6fec91689f796e468011668
Miguel Sanchez <msanchez@sertracen.com>
2026-01-26 18:04:04 -0600

    se hace mantenimiento de actividades economicas

---
+++ src/main/java/sv/com/jsoft/efactmh/model/ActividadEconomica.java
@@ -0,0 +1,15 @@
+package sv.com.jsoft.efactmh.model;
+
+import lombok.Data;
+
+/**
+ *
+ * @author msanchez
+ */
+@Data
+public class ActividadEconomica {
+
+    private String codigoActividad;
+    private String actividadEconomica;
+    private boolean principal;
+}
